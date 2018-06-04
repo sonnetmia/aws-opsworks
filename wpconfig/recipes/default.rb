27ad8168d6b7e091f126867c0365ef0429b1bd3e
Md. Ariful Islam <aisonnet01@gmail.com>
2018-06-04 19:56:49 +0600

    action value defined

--- wpconfig/recipes/default.rb
+++ wpconfig/recipes/default.rb
@@ -4,6 +4,7 @@ template "/srv/#{app['shortname']}/wp-config.php" do
 	owner 'www-data'
 	group 'www-data'
 	mode '0640'
+	action :create
 	variables(
 		:database   => '#{app['environment']['testvalue']}'
 	)
