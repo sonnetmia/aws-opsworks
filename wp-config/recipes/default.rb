68343549991b5a766b005d2489c28519cb59aefc
Md. Ariful Islam <aisonnet01@gmail.com>
2018-06-04 14:54:45 +0600

    fixing parse value

--- wp-config/recipes/default.rb
+++ wp-config/recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+app = search("aws_opsworks_app").first
+Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
