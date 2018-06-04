05df2805b4ae896a9c4dadffb8f0392d2816e391
Md. Ariful Islam <aisonnet01@gmail.com>
2018-06-04 15:08:56 +0600

    stack info

--- wp-config/recipes/default.rb
+++ wp-config/recipes/default.rb
@@ -7,3 +7,10 @@
 app = search("aws_opsworks_app").first
 Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
 Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+
+
+data_bag("aws_opsworks_app").each do |data_bag_item|
+  app = data_bag_item("aws_opsworks_app", data_bag_item)
+  Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+  Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+end
