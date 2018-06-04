89f8222731a8c54e158a20c4fe21227f109a9773
Md. Ariful Islam <aisonnet01@gmail.com>
2018-06-04 19:49:35 +0600

    Fixed clean deployment

--- wp-config/recipes/default.rb
+++ wp-config/recipes/default.rb
@@ -4,16 +4,24 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+#data_bag("aws_opsworks_app").each do |data_bag_item|
+#  app = data_bag_item("aws_opsworks_app", data_bag_item)
+#  Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+#  Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+#end
+
 app = search("aws_opsworks_app").first
 Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
 Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+Chef::Log.info("********** The app's URL is '#{app['environment']['testvalue']}' **********")
 
-
-data_bag("aws_opsworks_app").each do |data_bag_item|
-  app = data_bag_item("aws_opsworks_app", data_bag_item)
-  Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
-  Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
-  Chef::Log.info("********** The app's URL is '#{app['environment']['testvalue']}' **********")
+template "/srv/#{app['shortname']}/wp-config.php" do
+	source "wp-config.php.erb"
+	owner 'www-data'
+	group 'www-data'
+	mode '0640'
+	variables(
+		:database   => '#{app['environment']['testvalue']}'
+	)
 end
-
 
--- wpconfig/recipes/default.rb
+++ wpconfig/recipes/default.rb
@@ -1,16 +1,11 @@
 app = search(:aws_opsworks_app).first
 template "/srv/#{app['shortname']}/wp-config.php" do
-  source "wp-config.php.erb"
-  owner 'www-data'
-  group 'www-data'
-  mode '0640'
+	source "wp-config.php.erb"
+	owner 'www-data'
+	group 'www-data'
+	mode '0640'
+	variables(
+		:database   => '#{app['environment']['testvalue']}'
+	)
 end
 
-
-#cookbook_file "/srv/wordpress/wp-config.php" do
-##  source "wp-config.php"
-#  mode '0640'
-#  action :create
-#  owner 'www-data'
-#  group 'www-data'
-#end
