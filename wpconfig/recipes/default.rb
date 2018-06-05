a6fd7d109d766bdd6657a5291de54c16a8ddd372
Md. Ariful Islam <aisonnet01@gmail.com>
2018-06-05 09:04:24 +0600

    envvar

--- wpconfig/recipes/default.rb
+++ wpconfig/recipes/default.rb
@@ -4,10 +4,23 @@ template "/srv/#{app['shortname']}/wp-config.php" do
 	owner 'www-data'
 	group 'www-data'
 	mode '0640'
-	action :create
-    variables(
-        :database   => ('#{app['environment']['testvalue']}'),
-        :user       => ('#{app['environment']['testvalue']}')
-    )	
+	action [:delete, :create]
+	variables(
+	    :db_name          =>  "#{app['environment']['DATABASE_NAME']}"
+	    #:db_user          =>  "#{app['environment']['DATABASE_USER']}",
+	    #:db_password      =>  "#{app['environment']['DATABASE_PASWORD']}",	    	    
+	    #:db_host          =>  "#{app['environment']['DATABASE_HOST']}",
+	    #:wp_env           =>  "#{app['environment']['WP_ENV']}",
+	    #:wp_home          =>  "#{wp_home}",
+	    #:wp_siteurl       =>  "#{site_url}",
+	    #:auth_key         =>  "#{app['environment']['AUTH_KEY']}",
+	    #:secure_auth_key  =>  "#{app['environment']['SECURE_AUTH_KEY']}",
+	    #:logged_in_key    =>  "#{app['environment']['LOGGED_IN_KEY']}",
+	    #:nonce_key        =>  "#{app['environment']['NONCE_KEY']}",
+	    #:auth_salt        =>  "#{app['environment']['AUTH_SALT']}",
+	    #:secure_auth_salt =>  "#{app['environment']['SECURE_AUTH_SALT']}",
+	    #:logged_in_salt   =>  "#{app['environment']['LOGGED_IN_SALT']}",
+	    #:nonce_salt       =>  "#{app['environment']['NONCE_SALT']}"
+	)    	
 end
 
